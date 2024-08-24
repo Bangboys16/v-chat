4add74db3a46fbae61a719f863128d0472200e22
Edidiong Eka <edidiongeka54@gmail.com>
2024-08-24 13:28:05 +0100

    necessary changes

--- vigichat/settings.py
+++ vigichat/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yghrqzzgqf096d$ujaa0+a88o0s6%bo!be!b@osq353f@kv8l1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Application definition
 
